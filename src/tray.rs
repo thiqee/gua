397509aea3b6818dc3cfdb4258b608ad21058bd6
Gliguala <1198236721@qq.com>
2026-06-14 09:42:07 +0800

    feat: 支持自定义托盘图标，exe 目录下放 KeyHop.ico 即可替换

--- src/tray.rs
+++ src/tray.rs
@@ -3,6 +3,7 @@
 #![allow(unused_must_use)]
 
 use std::ptr;
+use std::os::windows::ffi::OsStrExt;
 use windows::core::{w, PCWSTR};
 use windows::Win32::Foundation::*;
 use windows::Win32::UI::Shell::*;
@@ -18,6 +19,18 @@ static mut TRAY_HWND: HWND = HWND(ptr::null_mut());
 
 pub unsafe fn init(hwnd: HWND) {
     TRAY_HWND = hwnd;
+
+    // 尝试加载 exe 目录下的 KeyHop.ico
+    let custom_icon = std::env::current_exe()
+        .ok()
+        .map(|p| p.with_file_name("KeyHop.ico"))
+        .filter(|p| p.exists())
+        .and_then(|ico| {
+            let ws: Vec<u16> = ico.as_os_str().encode_wide().chain(Some(0)).collect();
+            LoadImageW(None, PCWSTR(ws.as_ptr()), IMAGE_ICON, 0, 0, LR_LOADFROMFILE).ok()
+        })
+        .map(|h| HICON(h.0));
+
     let mut nid = NOTIFYICONDATAW {
         cbSize: size_of::<NOTIFYICONDATAW>() as u32,
         hWnd: hwnd,
@@ -26,7 +39,7 @@ pub unsafe fn init(hwnd: HWND) {
         uCallbackMessage: TRAY_MSG,
         ..Default::default()
     };
-    nid.hIcon = LoadIconW(None, IDI_APPLICATION).unwrap_or_default();
+    nid.hIcon = custom_icon.unwrap_or_else(|| LoadIconW(None, IDI_APPLICATION).unwrap_or_default());
     let tip: Vec<u16> = "KeyHop\0".encode_utf16().collect();
     for (i, &c) in tip.iter().enumerate() {
         if i < nid.szTip.len() {
